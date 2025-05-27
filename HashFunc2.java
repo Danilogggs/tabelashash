ee07a73e738b22ee017cd23958302520f28b6251
Danilo <dangggs@gmail.com>
2025-05-27 00:11:06 -0300

    vF

--- HashFunc2.java
+++ HashFunc2.java
@@ -3,7 +3,7 @@ public class HashFunc2 extends HashTable {
     public int hash(String key) {
         int hash = 0;
         for (int i = 0; i < key.length(); i++) {
-            hash = key.charAt(i) * (i + 1);
+            hash = hash^4 * key.charAt(i) * (i + 1);
         }
         return hash % size;
     }
--- Main.java
+++ Main.java
@@ -7,34 +7,58 @@ public class Main {
         HashTable table1 = new HashFunc1();
         HashTable table2 = new HashFunc2();
 
+        // Tempo inserção tabela 1
+        long tempo1S = System.currentTimeMillis();
         try (BufferedReader reader = new BufferedReader(new FileReader("female_names.txt"))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 table1.insert(line);
+            }
+        }
+        long tempo1F = System.currentTimeMillis();
+
+        // Tempo inserção tabela 2
+        long tempo2S = System.currentTimeMillis();
+        try (BufferedReader reader = new BufferedReader(new FileReader("female_names.txt"))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
                 table2.insert(line);
             }
         }
+        long tempo2F = System.currentTimeMillis();
 
         String[] testNames = {"Josue", "Emily", "Charlotte", "Zoe", "Megan"};
 
-        System.out.println("\nResultados da busca na Tabela 1:");
+        //  Tempo tabela 1
+        System.out.println("\nbusca na Tabela 1:");
+        long tempoBusca1S = System.currentTimeMillis();
         for (String name : testNames) {
             boolean found = table1.search(name);
-            System.out.println("Nome " + name + " encontrado? " + (found ? "Sim" : "N\u00e3o"));
+            System.out.println("Nome " + name + " encontrado? " + (found ? "Sim" : "NÃo"));
         }
+        long tempoBusca1F = System.currentTimeMillis();
 
-        System.out.println("\nResultados da busca na Tabela 2:");
+        // Tempo tabela 2
+        System.out.println("\nbusca na Tabela 2:");
+        long tempoBusca2S = System.currentTimeMillis();
         for (String name : testNames) {
             boolean found = table2.search(name);
-            System.out.println("Nome " + name + " encontrado? " + (found ? "Sim" : "N\u00e3o"));
+            System.out.println("Nome " + name + " encontrado? " + (found ? "Sim" : "NÃo"));
         }
+        long tempoBusca2F = System.currentTimeMillis();
 
-        System.out.println("\n--- TABELA 1");
+        // tabela 1
+        System.out.println("\nTABELA 1");
         System.out.println("Colisões: " + table1.getCollisions());
+        System.out.println("Tempo de inserção: " + (tempo1F - tempo1S) + " ms");
+        System.out.println("Tempo de busca: " + (tempoBusca1F - tempoBusca1S) + " ms");
         table1.printDistribution();
 
-        System.out.println("\n--- TABELA 2");
+        // tabela 2
+        System.out.println("\nTABELA 2");
         System.out.println("Colisões: " + table2.getCollisions());
+        System.out.println("Tempo de inserção: " + (tempo2F - tempo2S) + " ms");
+        System.out.println("Tempo de busca: " + (tempoBusca2F - tempoBusca2S) + " ms");
         table2.printDistribution();
     }
 }
